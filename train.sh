6ed32c08c29e8f0e318afe2e83850234395a9258
B4rb4r1s <vgokarev@mail.ru>
2026-03-09 13:06:35 +0300

    utility update (3)

--- train.sh
+++ train.sh
@@ -1,5 +1,9 @@
 echo "====== MISM ======"
 echo "=== Количество доступных GPU ==="
 nvidia-smi -L | wc -l
+
+echo "=== Разбиение датасета ==="
+python scripts/prepare_data.py --input dataset/dataset-SM-17k.json --output dataset/splits --min_summary_len 100 --val_ratio 0.10 --test_ratio 0.10 --seed 42
+
 echo "=== Начало обучения ==="
-CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 torchrun --nproc_per_node=6 scripts/train.py --config configs/gazeta_2stage.yaml
+torchrun --nproc_per_node=6 scripts/train.py --config configs/gazeta_2stage.yaml 2>&1 | tee ./tmp/debug.log
