3ccd45eff68f99292e4dac8b7354ee1d258d9405
B4rb4r1s <vgokarev@mail.ru>
2026-03-16 23:41:47 +0300

    utility update (5)

--- train.sh
+++ train.sh
@@ -6,4 +6,5 @@ echo "=== Разбиение датасета ==="
 python scripts/prepare_data.py --input dataset/dataset-SM-17k.json --output dataset/splits --min_summary_len 100 --val_ratio 0.10 --test_ratio 0.10 --seed 42
 
 echo "=== Начало обучения ==="
-torchrun --nproc_per_node=6 scripts/train.py --config configs/gazeta_2stage.yaml 2>&1 | tee ./tmp/debug.log
+# torchrun --nproc_per_node=6 scripts/train.py --config configs/gazeta_2stage.yaml 2>&1 | tee ./tmp/debug.log
+torchrun --nproc_per_node=6 scripts/train.py --config configs/fred_2stage.yaml 2>&1 | tee ./tmp/debug.log
